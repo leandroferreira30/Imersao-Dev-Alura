70ec19239d2a42a95394b8a191df4d6c142cf325
leandroferreira30 <96580968+leandroferreira30@users.noreply.github.com>
2024-09-04 14:44:00 -0300

    Aula 3
    Pondo em prática o que foi visto na aula 3 da Imersão Dev

---
+++ dados.js
@@ -0,0 +1,82 @@
+let dados = [
+  {
+    nome: "Charles Leclerc",
+    nacionalidade: "Monegásco",
+    equipe: "Ferrari",
+    descricao:
+      "Charles Leclerc, nascido em Mônaco, é um dos pilotos mais promissores da atual geração da Fórmula 1. Conhecido por sua habilidade, velocidade e determinação, Leclerc conquistou o coração dos fãs da Ferrari ao demonstrar um talento excepcional desde sua estreia na categoria. Sua ascensão meteórica, marcada por vitórias memoráveis e duelos acirrados com os principais adversários, o posiciona como um dos favoritos para conquistar títulos mundiais nos próximos anos. Com uma paixão pela velocidade desde a infância, Leclerc carrega consigo a responsabilidade de representar uma das equipes mais icônicas da história da Fórmula 1, a Ferrari, e de continuar escrevendo um novo capítulo na história do automobilismo mundial.",
+    link: "https://pt.wikipedia.org/wiki/Charles_Leclerc",
+  },
+  {
+    nome: "Lando Norris",
+    nacionalidade: "Britânico",
+    equipe: "McLaren",
+    descricao:
+      "Lando Norris, um jovem talento britânico que vem se destacando na Fórmula 1. Conhecido por seu estilo de pilotagem agressivo e carisma, Norris conquistou a admiração dos fãs da McLaren desde sua estreia. Sua juventude e habilidade o colocam como um dos pilotos mais promissores da nova geração. Com um futuro promissor pela frente, Norris é considerado um dos principais candidatos a conquistar títulos mundiais nos próximos anos.",
+    link: "https://pt.wikipedia.org/wiki/Lando_Norris",
+  },
+  {
+    nome: "Lewis Hamilton",
+    nacionalidade: "Britânico",
+    equipe: "Mercedes",
+    descricao:
+      "Lewis Hamilton, um ícone da Fórmula 1. Lewis Hamilton é um dos maiores pilotos da história da categoria. Com múltiplos títulos mundiais e um talento inegável, Hamilton se tornou um símbolo do esporte. Sua rivalidade com outros grandes pilotos, como Sebastian Vettel e Max Verstappen, marcou época na Fórmula 1. Sua habilidade no volante, combinada com sua personalidade carismática e engajamento em causas sociais, fazem de Hamilton uma figura inspiradora para milhões de fãs ao redor do mundo.",
+    link: "https://pt.wikipedia.org/wiki/Lewis_Hamilton",
+  },
+  {
+    nome: "Max Verstappen",
+    nacionalidade: "Holandesa",
+    equipe: "Red Bull",
+    descricao:
+      "Max Verstappen, o piloto holandês, emergiu como uma das maiores forças da Fórmula 1 na última década. Com um estilo de pilotagem agressivo e uma habilidade impressionante de ultrapassagem, Verstappen conquistou o coração dos fãs e se tornou um dos nomes mais comentados do esporte. Desde sua estreia precoce na categoria, o holandês demonstrou um talento natural e uma maturidade incomum para sua idade, o que o levou a conquistar títulos mundiais e a estabelecer diversos recordes. Sua rivalidade com Lewis Hamilton, que culminou em um emocionante campeonato de 2021, consolidou seu status como um dos maiores talentos da história da Fórmula 1",
+    link: "https://pt.wikipedia.org/wiki/Max_Verstappen",
+  },
+  {
+    nome: "Carlos Sainz Jr",
+    nacionalidade: "Espanhola",
+    equipe: "Ferrari",
+    descricao:
+      "Carlos Sainz Jr., filho do lendário campeão mundial de rali Carlos Sainz, é um piloto espanhol que se destaca na Fórmula 1 por sua consistência, habilidade e sangue frio. Desde sua estreia na categoria, Sainz tem impressionado pela sua capacidade de extrair o máximo de seus carros e por suas batalhas acirradas na pista. Com uma carreira sólida e em constante evolução, o espanhol se tornou um dos pilotos mais queridos e respeitados do grid, sendo um dos pilares da Scuderia Ferrari. Sua experiência, aliada ao seu talento natural, o coloca como um forte candidato a conquistar títulos mundiais nos próximos anos.",
+    link: "https://pt.wikipedia.org/wiki/Carlos_Sainz",
+  },
+  {
+    nome: "George Russell",
+    nacionalidade: "Britânico",
+    equipe: "Mercedes",
+    descricao:
+      "George Russell, piloto britânico da Mercedes, tem se destacado como uma das maiores promessas da nova geração da Fórmula 1. Desde sua estreia, tem impressionado pela sua consistência, habilidade e maturidade. Considerado um dos pilotos mais rápidos e talentosos do grid, Russell já conquistou uma vitória na Fórmula 1 e tem demonstrado um grande potencial para alcançar ainda mais sucesso na categoria. Sua ascensão meteórica, aliada à sua parceria com Lewis Hamilton, promete um futuro brilhante para o piloto britânico.",
+    link: "https://pt.wikipedia.org/wiki/George_Russell",
+  },
+  {
+    nome: "Sergio Pérez",
+    nacionalidade: "Mexicana",
+    equipe: "Red Bull",
+    descricao:
+      "Sergio Pérez, conhecido carinhosamente como 'Checo Pérez', é um piloto mexicano que se destaca na Fórmula 1 por sua experiência e consistência. Com uma longa trajetória na categoria, Pérez é conhecido por sua habilidade em extrair o máximo de seus carros e por sua capacidade de trabalhar em equipe. O mexicano tem se mostrado um forte aliado para a Red Bull, contribuindo para os sucessos da equipe nas últimas temporadas. Sua habilidade em ultrapassagens e em condições de corrida desafiadoras o tornaram um dos pilotos mais respeitados do grid.",
+    link: "https://pt.wikipedia.org/wiki/Sergio_Pérez",
+  },
+  {
+    nome: "Oscar Piastri",
+    nacionalidade: "Australiana",
+    equipe: "McLaren",
+    descricao:
+      "Oscar Piastri, o jovem australiano, representa uma nova geração de pilotos na Fórmula 1. Campeão da Fórmula 2 em 2021, Piastri chegou à categoria máxima do automobilismo com grande expectativa, e até agora tem mostrado um talento promissor. Sua adaptação ao carro da McLaren tem sido rápida, e o australiano já demonstra um estilo de pilotagem preciso e consistente. Com um futuro brilhante pela frente, Piastri é um dos nomes a se acompanhar de perto nos próximos anos.",
+    link: "https://pt.wikipedia.org/wiki/Oscar_Piastri",
+  },
+  {
+    nome: "Pierre Gasly",
+    nacionalidade: "Francês",
+    equipe: "Alpine",
+    descricao:
+      "Pierre Gasly, piloto francês com uma trajetória sólida na Fórmula 1. Conhecido por sua habilidade de ultrapassagem e adaptabilidade, Gasly já conquistou um pódio na Fórmula 1 e demonstrou ser um piloto rápido e consistente. Sua experiência e maturidade o tornam um forte competidor no grid. Com uma carreira em constante evolução, Gasly busca consolidar seu lugar entre os melhores pilotos da categoria.",
+    link: "https://pt.wikipedia.org/wiki/Pierre_Gasly",
+  },
+  {
+    nome: "Esteban Ocon",
+    nacionalidade: "Francês",
+    equipe: "Alpine",
+    descricao:
+      "Esteban Ocon, piloto francês com uma carreira promissora na Fórmula 1. Conhecido por sua técnica apurada e consistência, Ocon já conquistou um pódio na Fórmula 1 e demonstrou ser um piloto rápido e estratégico. Sua experiência na equipe reserva o torna um forte aliado para o desenvolvimento do carro. Com uma carreira em constante ascensão, Ocon busca conquistar títulos mundiais e consolidar seu lugar entre os grandes nomes da Fórmula 1.",
+    link: "https://pt.wikipedia.org/wiki/Esteban_Ocon",
+  },
+];
